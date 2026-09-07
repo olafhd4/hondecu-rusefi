6dd0ec2bc6353ac837a3637eeee81e0686724641
olafh <olafh@komp-olafowy.local>
2026-09-07 12:16:56 +0200

    sdioo

--- board.mk
+++ board.mk
@@ -23,4 +23,4 @@ DDEFS += -DEFI_WIDEBAND_FIRMWARE_UPDATE=FALSE
 # Wymuszenie sprzętowego sterownika SDIO (zamiast powolnego SPI)
 DDEFS += -DHAL_USE_SDC=TRUE
 DDEFS += -DEFI_SDC_DEVICE=SDCD1
-DDEFS += -Dboard_sdc_is_inserted()=true
+DDEFS += -D"board_sdc_is_inserted()=true"
